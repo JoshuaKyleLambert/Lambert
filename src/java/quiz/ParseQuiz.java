a150c2df7be319f1e112f03093244abc8cc10793
Joshua <jl6356@stu.armstrong.edu>
2018-02-18 18:27:26 -0500

    nulls fixed all 1280 tuples import to the database .

--- src/java/quiz/ParseQuiz.java
+++ src/java/quiz/ParseQuiz.java
@@ -8,6 +8,10 @@ package quiz;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -18,9 +22,10 @@ import java.util.regex.Pattern;
  */
 public class ParseQuiz {
 
+    private PreparedStatement insertStatement;
     final int CHAPTERS = 44;
     private int chapter;
-   // String line;
+    // String line;
 
     public static void main(String[] args) {
         ParseQuiz quizlist = new ParseQuiz();
@@ -28,10 +33,66 @@ public class ParseQuiz {
     }
 
     public ParseQuiz() {
-        printQuestionList();
+        initDB();
+        readFiles();
     }
 
-    public void printQuestionList() {
+    private void initDB() {
+
+        try {
+            //Load the JDBC driver
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("Driver loaded");
+
+            //Establish a connection
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/javabook", "scott", "tiger");
+            System.out.println("Database connected");
+
+            //String selectString = "SELECT  lastName, mi, firstName, address, city, "
+            //        + " state, telephone, email, id FROM Staff WHERE Staff.id = ?";
+            String insertString = "INSERT INTO intro11equiz (chapterNo, questionNO, question, choiceA,"
+                    + " choiceB, choiceC, choiceD, choiceE, answerKey, hint)"
+                    + " VALUES (?,?,?,?,?,?,?,?,?,?)";
+
+//            String updateString = "UPDATE Staff SET "
+//                    + "id=?, lastName=?, mi=?, firstName=?, address=?, city=?, state=?,"
+//                    + "telephone=?, email=? WHERE id=?";
+            //  selectStatement = connection.prepareStatement(selectString);
+            insertStatement = connection.prepareStatement(insertString);
+            // updateStatement = connection.prepareStatement(updateString);
+
+        } catch (ClassNotFoundException | SQLException ex) {
+            //setError(ex.getMessage());
+        }
+    }
+
+    private void importToDB(Question question) {
+        try {
+
+            //if (idNumber.contains(" ") || idNumber.equals("")) {
+            //  throw new SQLException("ID must not be left blank or contain spaces!");
+            // }
+            insertStatement.setInt(1, question.chapterNo);
+            insertStatement.setInt(2, question.questionNo);
+            insertStatement.setString(3, question.questionText);
+            insertStatement.setString(4, question.choiceA);
+            insertStatement.setString(5, question.choiceB);
+            insertStatement.setString(6, question.choiceC);
+            insertStatement.setString(7, question.choiceD);
+            insertStatement.setString(8, question.choiceE);
+            insertStatement.setString(9, question.answerKey);
+            insertStatement.setString(10, question.hint);
+            insertStatement.execute();
+
+            //setError("Record ID: " + idNumber + " insert success!");
+        } catch (SQLException ex) {
+
+            //setError(ex.getMessage());
+        }
+
+    }
+
+    public void readFiles() {
         ArrayList read = null;
         int counter = 0;
         for (int i = 1; i <= CHAPTERS; i++) {
@@ -39,8 +100,9 @@ public class ParseQuiz {
             read = readQuestions("C:\\selftest\\selftest11e\\chapter" + i + ".txt");
             //read.forEach(e -> System.out.println(e.toString()));
             //read.forEach(e -> import(e));
+            read.forEach(e -> importToDB((Question) e));
             System.out.printf("%d\n", counter += read.size());
-            
+
         }
 
     }
@@ -81,10 +143,14 @@ public class ParseQuiz {
      */
     private ArrayList<Question> readQuestions(String filename) {
         String matchThis = "\\d{1,2}";
-        Pattern pattern = Pattern.compile("\\d{1,2}\\.\\s+(.*)");
+        Pattern pattern = Pattern.compile("\\d{1,2}\\.\\s*(.*)$");
         Pattern answers = Pattern.compile("^[Kk]ey:([a-e]{1,5})\\s*(.*)$");//^[kK]ey:([a-e]+)\s*(.*)$
         Pattern choice = Pattern.compile("^[a-eA-E]\\.\\s*(.*)$");
+        Pattern choiceA = Pattern.compile("^\\s*[Aa]\\.");
+        Pattern newQuestion = Pattern.compile("#{1}\\s");
         Matcher matcher;
+        Matcher questionMatcher;
+        Matcher choiceAMatcher;
         Matcher choiceMatcher;
         String line;
         ArrayList<Question> questionList = new ArrayList<>();
@@ -92,9 +158,9 @@ public class ParseQuiz {
 
         try {
             BufferedReader input = new BufferedReader(new FileReader(filename));
-
-            input.readLine();// Skip 
-            input.readLine();// Skip 
+            
+            //input.readLine();// Skip 
+            //input.readLine();// Skip 
             input.readLine();// 4th line is the first real line of questions
 
             while ((line = input.readLine()) != null) {
@@ -106,13 +172,17 @@ public class ParseQuiz {
                 if (line.startsWith("Section")) {
                     line = input.readLine(); //Discard section Lines for now.
                 }
-                while (!line.equals("#")) {
+                while (!line.startsWith("#")) {
+                    questionMatcher = newQuestion.matcher(line);
                     matcher = pattern.matcher(line);
                     choiceMatcher = choice.matcher(line);
-
+                    choiceAMatcher = choice.matcher(line);
+                    
                     if (matcher.matches()) {
                         description.append(matcher.group(1));
                         line = input.readLine();
+                    } else if (line.startsWith("Section")) {
+                        line = input.readLine(); //Discard section lines.
                     } else if (line.startsWith("a.") || line.startsWith("A.")) {
                         choiceMatcher.matches();
                         question.choiceA = choiceMatcher.group(1);
