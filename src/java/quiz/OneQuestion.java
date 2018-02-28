67bb4fe1acb086d8a089ecbb23cb8cc476fa5ad1
Joshua <jl6356@stu.armstrong.edu>
2018-02-28 10:46:55 -0500

    insert user answers to database working

--- src/java/quiz/OneQuestion.java
+++ src/java/quiz/OneQuestion.java
@@ -5,12 +5,12 @@
  */
 package quiz;
 
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Arrays;
 
 /**
  *
@@ -25,10 +25,21 @@ public class OneQuestion implements java.io.Serializable {
     String choiceB = "";
     String choiceC = "";
     String choiceD = "";
-    String choiceE ;
+    String choiceE;
     String answerKey;
     String hint;
-    String[] answerList = {"","","","",""};
+    String[] answerList = {"", "", "", "", ""};
+    String buttonName;
+    String[] submittedAnswers = {"x"};
+
+    Boolean answerA = false;
+    Boolean answerB = false;
+    Boolean answerC = false;
+    Boolean answerD = false;
+    Boolean answerE = false;
+    String hostName;
+    Boolean isCorrect;
+    String username = "Anonymous";
 
     private PreparedStatement selectStatement;
     private PreparedStatement insertStatement;
@@ -71,6 +82,30 @@ public class OneQuestion implements java.io.Serializable {
 
     }
 
+    private void insertRecord() {
+
+        try {
+
+            insertStatement.setString(1, chapterNo);
+            insertStatement.setString(2, questionNo);
+            insertStatement.setBoolean(3, isCorrect);
+            //insertStatement.setString(4, time);
+            insertStatement.setString(4, hostName);
+            insertStatement.setBoolean(5, answerA);
+            insertStatement.setBoolean(6, answerB);
+            insertStatement.setBoolean(7, answerC);
+            insertStatement.setBoolean(8, answerD);
+            insertStatement.setBoolean(9, answerE);
+            insertStatement.setString(10, username);
+            insertStatement.execute();
+
+        } catch (SQLException ex) {
+
+            System.out.println(ex.getMessage());
+        }
+
+    }
+
     private void initializeDB() {
 
         try {
@@ -85,9 +120,9 @@ public class OneQuestion implements java.io.Serializable {
             String selectString = "SELECT  question, choiceA, choiceB, choiceC, choiceD, choiceE, answerKey, hint "
                     + "  FROM intro11equiz WHERE chapterNo = ? AND questionNo = ?";
 
-            String insertString = "INSERT INTO intro11e (chapterNo, questionNo, isCorrect, time,"
+            String insertString = "INSERT INTO intro11e (chapterNo, questionNo, isCorrect,"
                     + " hostname, answerA, answerB, answerC, answerD, answerE, username)"
-                    + " VALUES (?,?,?,?,?,?,?,?,?,?,?)";
+                    + " VALUES (?,?,?,?,?,?,?,?,?,?)";
 
             String updateString = "UPDATE Staff SET "
                     + "id=?, lastName=?, mi=?, firstName=?, address=?, city=?, state=?,"
@@ -103,13 +138,87 @@ public class OneQuestion implements java.io.Serializable {
 
     }
 
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getHostName() {
+        return hostName;
+    }
+
+    public void setHostName(String hostName) {
+        this.hostName = hostName;
+    }
+
+    public Boolean getIsCorrect() {
+        return isCorrect;
+    }
+
+    public void setIsCorrect(String isCorrect) {
+        System.out.println(isCorrect);
+        
+        //this.isCorrect = isCorrect;
+    }
+
+    public String[] getSubmittedAnswers() {
+        return submittedAnswers;
+    }
+
+    public void setSubmittedAnswers(String[] submittedAnswers) {
+
+        this.submittedAnswers = submittedAnswers;
+
+    }
+
+    public String getButtonName() {
+        return buttonName;
+    }
+
+    public void setButtonName(String buttonName) {
+        this.buttonName = buttonName;
+//        System.out.println("Answers: " + Arrays.toString(answerList).contains(Arrays.toString(submittedAnswers)));
+//        System.out.println("AnswerList: " )
+        StringBuilder check = new StringBuilder();
+        if (getSubmittedAnswers() != null) {
+            for (String e : getSubmittedAnswers()) {
+                check.append(e);
+                switch (e) {
+                    case "a":
+                        answerA = true;
+                        break;
+                    case "b":
+                        answerB = true;
+                        break;
+                    case "c":
+                        answerC = true;
+                        break;
+                    case "d":
+                        answerD = true;
+                        break;
+                    case "e":
+                        answerE = true;
+                        break;
+
+                }
+
+            }
+        }
+        isCorrect = check.toString().equals(getAnswerKey());
+               
+        insertRecord();
+    }
+
     public String[] getAnswerList() {
         String[] answerList1 = {choiceA, choiceB, choiceC, choiceD, choiceE};
-        
+
         for (String e : answerList1) {
             System.out.println(e);
         }
-        
+
         answerList = answerList1;
         return answerList;
     }
